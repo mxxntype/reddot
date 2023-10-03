713ad5e63f9d80a85745721e58f19f5f24ff14d6
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-10-03 16:48:47 +0300

    chore: Fix typo

--- src/cli.rs
+++ src/cli.rs
@@ -5,7 +5,7 @@ pub struct Args {
     /// Pattern by which the executables will be filtered
     #[arg(short, long)]
     pub pattern: String,
-    /// Maximum count of executables in ouput
+    /// Maximum count of executables in output
     #[arg(short, long)]
     pub exe_count: usize,
 }
