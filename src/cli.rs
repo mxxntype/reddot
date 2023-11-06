57d167b445a56984109af523a21d16a67fb892a8
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-11-06 16:50:08 +0300

    fix: Filter out hidden files Rename `Config::exe_count` to `Config::max_files`

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,11 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-pub struct Args {
+pub struct Config {
     /// Pattern by which the executables will be filtered
     #[clap(short, long)]
     pub pattern: String,
     /// Maximum count of executables in output
     #[clap(short, long)]
-    pub exe_count: usize,
+    pub max_files: usize,
 }
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::{env, fs};
 
 mod cli;
 use clap::Parser;
-use cli::Args;
+use cli::Config;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     let path = env::var("PATH")?;
-    let args = Args::parse();
+    let config = Config::parse();
 
     let mut executable_files: Vec<fs::DirEntry> = path
         .split(':')
@@ -17,11 +17,19 @@ fn main() -> color_eyre::Result<()> {
         .collect();
 
     executable_files.retain(|file: &fs::DirEntry| {
-        let filename = file.file_name().to_string_lossy().to_lowercase();
-        filename.contains(&args.pattern.to_lowercase()) && !filename.contains('.')
+        let filename = file.file_name();
+        let is_hidden = filename
+            .to_str()
+            .map(|s| s.starts_with('.'))
+            .unwrap_or(false);
+        let matches_pattern = filename
+            .to_string_lossy()
+            .to_lowercase()
+            .contains(&config.pattern.to_lowercase());
+        matches_pattern && !is_hidden
     });
 
-    executable_files.truncate(args.exe_count);
+    executable_files.truncate(config.max_files);
 
     print_filenames_json(&executable_files);
 
